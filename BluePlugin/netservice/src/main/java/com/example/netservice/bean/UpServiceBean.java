41c359fac3f816a634be6a3256f3d4b17b82d4e4
similar718 <1170214556@qq.com>
2019-10-11 18:46:58 +0800

    json

--- BluePlugin/netservice/src/main/java/com/example/netservice/bean/UpServiceBean.java
+++ BluePlugin/netservice/src/main/java/com/example/netservice/bean/UpServiceBean.java
@@ -1,5 +1,7 @@
 package com.example.netservice.bean;
 
 public class UpServiceBean {
-
+    /**
+     * {"Longitude":"104.43243","Latitude":30.321432","StartTime":13:25,"SerialNumber":32435325,"Temperature":"30℃","ElectricQuantity":"3.4V","Frequency ":1323}
+     */
 }
